7b113a15fa9a2d6321693c629ef5cee4283ef284
javadghasemi <javad.ghasemi8080@gmail.com>
2023-11-27 00:39:44 +0330

    Initialize database service with prisma ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
